478f28b546dee8665ec31b0df3baf3160758afcb
d.kanehira <disc99.g@gmail.com>
2017-03-03 17:31:54 +0900

    Change sh

--- tasks/deploy-s2-maven.sh
+++ tasks/deploy-s2-maven.sh
@@ -1,4 +1,4 @@
 #!/bin/bash                                                                                                                                                                                                
 echo "--"
-echo $1
+printenv
 echo "--"
